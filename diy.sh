dbd5bb631a373dd87fa7cab197aa6c24ba1ef501
alwin2010 <38851258+alwin2010@users.noreply.github.com>
2020-02-29 07:16:57 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #=================================================
-# Description: Thank you for using thr firmares made by sharingman.Please subscribe my youtube chanel https://www.youtube.com/channel/UCLWqki9mkVnCV9G8dx7ulsw
+# Description: Thank you for using the firmares made by sharingman.Please subscribe my youtube chanel https://www.youtube.com/channel/UCLWqki9mkVnCV9G8dx7ulsw
 # Lisence: MIT
 # Author: sharingman
 # Blog: http://sharingman.ga
